db91d1306b8d07bff36c594934e75ae1289efe27
juan cordoba <juancm1106.2@gmail.com>
2023-10-05 11:59:29 -0500

    Se mejoras en el diseño de la landing page

--- dllo-front-end-PrAu/pages/landing.vue
+++ dllo-front-end-PrAu/pages/landing.vue
@@ -1,7 +1,9 @@
 <template>
     <v-sheet class="main-p" >
-        <p>hola</p>
-
+        <v-card class="title" color="black">
+            <v-img class="banner-1" height="220" cover transition="fade-transition" color="black" src="https://www.hdwallpapers.in/download/lexus_lc_500_bespoke_red_car_carbon_2022_4k_hd_cars-HD.jpg" ></v-img>
+            <h1 style="margin: 1%;" class="title">Bienvenido a Bespoke Builds</h1>
+        </v-card>
     </v-sheet>
 </template>
 <style scoped>
@@ -10,8 +12,22 @@
     align-items: center;
     justify-content: center;
     min-height: 100vh;
-    background-color: #1d1a39;
+    background-color: #1A1F35;
     color: white;
+}
+
+.banner-1{
+    border-bottom-left-radius: 30px;
+    border-bottom-right-radius: 20px;
+
+}
+.title{
+    text-align: center;
+    margin: 0%;
+    background-color: black;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    font-family: 'Courier New', Courier, monospace;
 }
 
 </style>
