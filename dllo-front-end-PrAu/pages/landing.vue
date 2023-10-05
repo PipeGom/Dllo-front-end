3be2580a0aaa0f1b33fc750e79d5b886f51c000d
juan cordoba <juancm1106.2@gmail.com>
2023-10-05 11:06:58 -0500

    se creo la landing page

--- dllo-front-end-PrAu/pages/landing.vue
+++ dllo-front-end-PrAu/pages/landing.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Se logro</h1>
+    <v-sheet class="main-p" >
+        <p>hola</p>
+
+    </v-sheet>
 </template>
+<style scoped>
+.main-p{
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background-color: #1d1a39;
+    color: white;
+}
+
+</style>
