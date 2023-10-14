1794ed6b408cb379490d71c52fcfcfef0101a9d4
juan cordoba <juancm1106.2@gmail.com>
2023-10-14 17:37:58 -0500

    mejora del diseno pagina seguimiento

--- dllo-front-end-PrAu/pages/seguimiento-principal.vue
+++ dllo-front-end-PrAu/pages/seguimiento-principal.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container grid-list-md text-xs-center align-center fluid>
+    <v-container grid-list-md text-xs-center align-center fluid class="back">
         <v-layout class="lay-1">
             <v-btn prepend-icon="$vuetify" stacked variant="tonal"  class="boton" to="crear-seguimiento" >Crear Seguimiento</v-btn>
             <v-btn prepend-icon="$vuetify" stacked variant="tonal"  class="boton" to="actualizar-seguimiento" >Actualizar Seguimiento</v-btn>
@@ -27,10 +27,18 @@
     margin: 3%;
     border: 2px solid #230547;
     transition: background-color 0.4s;
+    background-color: rgba(255, 255, 255, 0.953);
 }
 .boton:hover{
     background-color: #230547;
     color:white;
+}
+
+.back{
+    background-image: url("../src/trackingBack.jpg");
+    background-size: cover; /* Establece el tamaño de fondo en "cover" */
+    background-position: center; /* Opcional: centra la imagen de fondo */
+  
 }
 
 </style>
