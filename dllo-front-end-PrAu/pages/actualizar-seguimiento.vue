f6c7efd40676851a1007e6f9d73ab43e9a644163
juan cordoba <juancm1106.2@gmail.com>
2023-10-16 18:18:20 -0500

    Feat: Creacion del listado de segumiento y edicion

--- dllo-front-end-PrAu/pages/actualizar-seguimiento.vue
+++ dllo-front-end-PrAu/pages/actualizar-seguimiento.vue
@@ -1,6 +1,17 @@
 <template>
-    <ListarSeguimiento/>
+    <v-sheet class="sheet1">
+        <ListarSeguimiento/>
+    </v-sheet>
 </template>
+<style scoped>
+.sheet1{
+    padding: 10%;
+    background-image: url(../src/creacionSeguimientoFondo.jpg);
+    background-size: cover; /* Establece el tamaño de fondo en "cover" */
+    background-position: center; /* Opcional: centra la imagen de fondo */
+    min-height: 100vh;
+}
+</style>
 <script>
 import ListarSeguimiento from '../components/ListarSeguimiento.vue';
 export default{
@@ -8,4 +19,8 @@ export default{
         ListarSeguimiento
     }
 }
+
+definePageMeta({
+layout: "blank",
+});
 </script>
