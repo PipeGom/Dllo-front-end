68eea01103c5c523c74c3d5a83396d74f0db91c6
JhonOlivares26 <89679333+JhonOlivares26@users.noreply.github.com>
2023-10-18 18:09:52 -0500

    Creación de la ventada modal para modificar articulo de inventarios y aplicación de estilos

--- dllo-front-end-PrAu/components/articulo_inventario.vue
+++ dllo-front-end-PrAu/components/articulo_inventario.vue
@@ -1,80 +1,112 @@
-import { Style } from '#build/components';
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-    <template v-slot:loader="{ isActive }">
-      <v-progress-linear
-        :active="isActive"
-        color="deep-purple"
-        height="4"
-        indeterminate
-      ></v-progress-linear>
-    </template>    
-    <v-img cover height="250" src="../src/Faro-delantero.jpeg"></v-img>
-
-    <v-card-item>
-        <v-row align="center">
-            <v-col cols="auto">
-                <v-card-title id="Titulo">Faro delantero</v-card-title>
-            </v-col>
-            <v-col cols="auto">
-                <v-card-title class="my-4 text-subtitle-1">$130.000</v-card-title>
-            </v-col>            
-        </v-row>      
-    </v-card-item>
-    <v-card-subtitle>
-        <span class="me-1">Ref 23124412</span>
-    </v-card-subtitle>
-
-    <v-card-text>
-      <div>
-        Los faros delanteros halógenos funcionan mediante el calentamiento de un
-        filamento de tungsteno dentro de una lámpara. Cuando la corriente
-        eléctrica pasa a través del filamento, se calienta y emite luz.
-      </div>
-    </v-card-text>
-
-    <v-divider class="mx-4 mb-1"></v-divider>
-
-    <v-card-title>
-      <v-row align="center">      
-        <v-col cols="auto">
-          Disponible: 3
-        </v-col>
-        <v-col cols="auto">
-          <v-btn icon size="small">
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-        </v-col>
-        <v-col cols="auto">
-          <v-btn icon size="small">
-            <v-icon>mdi-minus</v-icon>
-          </v-btn>
+  <div>
+    <h1 class="Title" style="text-align: center">Gestión de Inventario</h1>
+    <div>
+      <v-row>
+        <v-col
+          v-for="articulo in articulos" :key="articulo.Ref" cols="12" sm="6" md="4" lg="3">
+          <v-card :loading="loading" class="mx-auto my-4" style="height: 570px">
+            <template v-slot:loader="{ isActive }">
+              <v-progress-linear
+                :active="isActive"
+                color="deep-purple"
+                height="4"
+                indeterminate
+              ></v-progress-linear>
+            </template>
+            <v-img class="responsive-image" max-height="280" :src="articulo.imagen"></v-img>
+            <v-card-item>
+              <v-card-title id="Titulo">{{ articulo.nombre }}</v-card-title>
+            </v-card-item>
+            <v-card-subtitle>
+              <span class="me-1">Ref {{ articulo.Ref }}</span>
+            </v-card-subtitle>
+            <v-card-text>
+              <div>
+                {{ articulo.descripcion }}
+              </div>
+            </v-card-text>
+            <v-divider class="mx-4 mb-1"></v-divider>
+            <v-card-title>
+              <v-row align="center">
+                <v-col cols="auto"> Disponible: {{ articulo.cantidad }} </v-col>
+                <v-col cols="auto">
+                  <v-btn icon size="small">
+                    <v-icon>mdi-plus</v-icon>
+                  </v-btn>
+                </v-col>
+                <v-col cols="auto">
+                  <v-btn icon size="small">
+                    <v-icon>mdi-minus</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn color="deep-purple-lighten-2" variant="text" @click="EditarArticulo">
+                Editar Articulo
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-col>
       </v-row>
-    </v-card-title>
+    </div>
 
-    <v-card-actions>
-      <v-btn color="deep-purple-lighten-2" variant="text" @click="reserve">
-        Editar Articulo
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+    <v-dialog v-model="mostrarModal" max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="deep-purple-lighten-2" variant="text" v-on="on">
+          Editar Articulo
+        </v-btn>
+      </template>
+      <v-card>
+        <!-- Contenido del modal -->
+        <v-card-title>Editar Articulo</v-card-title>
+        <!-- Agrega aquí los campos y elementos para editar el artículo -->
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
+import { ref, onBeforeMount } from "vue";
 import axios from "axios";
-const articulos = ref([])
 
-onBeforeMount(() => {
-    CargarArticulos()
-})
+export default {
+  data() {
+    return {
+      loading: false,
+      mostrarModal: false
+    };
+  },
+  setup() {
+  const articulos = ref([]);
+  onBeforeMount(async () => {
+    await CargarArticulos(articulos);
+  });
 
-const CargarArticulos = async () => {
-    const url = "http://localhost:3000/inventario"
-    const { data } = await axios.get(url)
-    articulos.value = data
-    console.log("articulos:", articulos.value)
-}  
+  const CargarArticulos = async (articulos) => {
+    const url = "http://localhost:3000/inventario";
+    try {
+      const { data } = await axios.get(url);
+      articulos.value = data;
+      console.log("articulos:", articulos.value);
+    } catch (error) {
+      console.error("Error al cargar los artículos:", error);
+    }
+  }
+
+  const EditarArticulo = () => {
+    mostrarModal = true;
+    console.log("funciona")
+  }
+
+  return {
+    articulos,
+    EditarArticulo,
+    mostrarModal
+  }
+}
+}
 </script>
 
 <style>
--- dllo-front-end-PrAu/pages/gestion-inventario.vue
+++ dllo-front-end-PrAu/pages/gestion-inventario.vue
@@ -1,6 +1,132 @@
 <template>
     <div>
-        <h1 class="Title" style="text-align: center;">Gestión de Inventario</h1>
-        <ArticuloInventario2></ArticuloInventario2>
+      <h1 class="Title" style="text-align: center">Gestión de Inventario</h1>
+      <div>
+        <v-row>
+          <v-col
+            v-for="articulo in articulos" :key="articulo.Ref" cols="12" sm="6" md="4" lg="3">
+            <v-card :loading="loading" class="mx-auto my-4" style="height: 570px">
+              <template v-slot:loader="{ isActive }">
+                <v-progress-linear
+                  :active="isActive"
+                  color="deep-purple"
+                  height="4"
+                  indeterminate
+                ></v-progress-linear>
+              </template>
+              <v-img class="responsive-image" max-height="280" :src="articulo.imagen"></v-img>
+              <v-card-item>
+                <v-card-title id="Titulo">{{ articulo.nombre }}</v-card-title>
+              </v-card-item>
+              <v-card-subtitle>
+                <span class="me-1">Ref {{ articulo.Ref }}</span>
+              </v-card-subtitle>
+              <v-card-text>
+                <div>
+                  {{ articulo.descripcion }}
+                </div>
+              </v-card-text>
+              <v-divider class="mx-4 mb-1"></v-divider>
+              <v-card-title>
+                <v-row align="center">
+                  <v-col cols="auto"> Disponible: {{ articulo.cantidad }} </v-col>
+                  <v-col cols="auto">
+                    <v-btn icon size="small">
+                      <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                  </v-col>
+                  <v-col cols="auto">
+                    <v-btn icon size="small">
+                      <v-icon>mdi-minus</v-icon>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-card-title>
+              <v-card-actions>
+                <v-btn color="deep-purple-lighten-2" variant="text" @click="EditarArticulo(articulo)">
+                  Editar Articulo
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+  
+      <v-dialog v-model="mostrarModal" max-width="600px">
+        <v-card>
+          <v-card-title>Editar Articulo</v-card-title>
+            <v-card-text>
+              <!-- Muestra la información del artículo aquí -->
+              <v-text-field v-model="articuloSeleccionado.nombre" label="Nombre"></v-text-field>
+              <v-text-field v-model="articuloSeleccionado.Ref" label="Referencia"></v-text-field>
+              <v-text-field v-model="articuloSeleccionado.precio" label="Precio"></v-text-field>
+              <v-text-field v-model="articuloSeleccionado.cantidad" label="Cantidad"></v-text-field>
+              <v-text-field v-model="articuloSeleccionado.descripcion" label="Descripción"></v-text-field>
+              <!-- Agrega más campos según sea necesario -->
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="blue darken-1" text @click="cerrarModal">Cancelar</v-btn>
+              <v-btn color="blue darken-1" @click="guardarCambios">Guardar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
     </div>
-</template>
+  </template>
+  
+  <script>
+  import { ref, onBeforeMount } from "vue";
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        loading: false,
+        mostrarModal: false
+      };
+    },
+    setup() {
+    const articulos = ref([]);
+    const mostrarModal = ref(false);
+    const articuloSeleccionado = ref(null);
+
+    onBeforeMount(async () => {
+      await CargarArticulos(articulos);
+    });
+
+    const CargarArticulos = async (articulos) => {
+      const url = "http://localhost:3000/inventario";
+      try {
+        const { data } = await axios.get(url);
+        articulos.value = data;
+      } catch (error) {
+        console.error("Error al cargar los artículos:", error);
+      }
+    }
+
+    const EditarArticulo = (articulo) => {
+      articuloSeleccionado.value = articulo;
+      mostrarModal.value = true;
+    }
+
+    return {
+      articulos,
+      EditarArticulo,
+      mostrarModal,
+      articuloSeleccionado
+    };
+  }
+}
+</script>
+
+  <style>
+  #Titulo {
+    padding-right: 2.5cm;
+  }
+  #tituloo {
+    padding-bottom: 0.1px;
+  }
+  #tituloo2 {
+    padding-top: 0.1px;
+  }
+  </style>
+  
