828f0c3f96dee080e8ec66e8185a6d4a6bbcaeda
JhonOlivares26 <89679333+JhonOlivares26@users.noreply.github.com>
2023-10-24 22:33:59 -0500

    Creación de barra de busqueda para el inventario

---
+++ dllo-front-end-PrAu/components/buscar-articulo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="search"
+            label="Buscar articulo"
+            class="mb-3"
+            placeholder="Ingrese el nombre o id del articulo"
+            @input="filtrarAutos"
+        ></v-text-field>
+    </div>
+</template>
